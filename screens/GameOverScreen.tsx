143f711242c620af3eb5be649e18bb0b20fae356
SUPHOT NARAPONG <suphot.n@gmail.com>
2025-12-19 14:09:44 +0700

    Sytling and Text

--- screens/GameOverScreen.tsx
+++ screens/GameOverScreen.tsx
@@ -1,6 +1,8 @@
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+import PrimaryButton from '@/components/ui/PrimaryButton';
 import Title from '@/components/ui/Title';
 import Colors from '@/constants/colors';
-import { Image, StyleSheet, Text, View } from 'react-native';
 
 function GameOverScreen() {
   return (
@@ -9,7 +11,10 @@ function GameOverScreen() {
       <View style={styles.imageContraine}>
         <Image source={require('../assets/images/success.png')} style={styles.image} />
       </View>
-      <Text>Your phone needed X rounds to guess the number Y.</Text>
+      <Text style={styles.summaryText}>Your phone needed <Text style={styles.highlight}>X</Text> rounds to guess the number 
+        <Text style={styles.highlight}>Y</Text>.
+      </Text>
+      <PrimaryButton>Start New Game</PrimaryButton>
     </View>
   );
 }
@@ -35,5 +40,15 @@ const styles = StyleSheet.create({
   image: {
     width: '100%',
     height: '100%',
+  },
+  summaryText: {
+    fontFamily: 'open-sans',
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 24,
+  },
+  highlight: {
+    fontFamily: 'open-sans-bold',
+    color: Colors.primary500,
   },
 });
